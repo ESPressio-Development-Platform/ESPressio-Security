9debb86f79e1b80b27b61d42d4f49cd5c13c057c
Simon J Stuart <lakraven@gmail.com>
2026-08-20 16:28:38 +0200

    feat: add Security umbrella header

---
+++ src/ESPressio_Security.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ESPressio_SecurityTypes.hpp"
+#include "ESPressio_IAeadCipher.hpp"
+#include "ESPressio_AeadCipherRegistry.hpp"
+#include "ESPressio_IKeyProvider.hpp"
+#include "ESPressio_StaticKeyProvider.hpp"
+#include "ESPressio_IRandomSource.hpp"
+#include "ESPressio_ReplayWindow.hpp"
+#include "ESPressio_TransportSecurity.hpp"
+#include "ESPressio_ISecureTransportCarrier.hpp"
+#include "ESPressio_SecureTransportDecorator.hpp"
+#include "ESPressio_MbedTLSAead.hpp"
+#if defined(ARDUINO_ARCH_ESP32) || defined(ESP_PLATFORM)
+#include "ESPressio_ESP32RandomSource.hpp"
+#endif
