fa5a99cda53b0ab2c3061d18e5e6fa832377a514
Simon J Stuart <lakraven@gmail.com>
2026-08-20 16:40:08 +0200

    test: add mbedTLS CCM API stub

---
+++ tests/stubs/mbedtls/ccm.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <cstddef>
+#ifndef MBEDTLS_CIPHER_ID_AES
+#define MBEDTLS_CIPHER_ID_AES 1
+#endif
+typedef struct { int dummy; } mbedtls_ccm_context;
+inline void mbedtls_ccm_init(mbedtls_ccm_context*){}
+inline void mbedtls_ccm_free(mbedtls_ccm_context*){}
+inline int mbedtls_ccm_setkey(mbedtls_ccm_context*,int,const unsigned char*,unsigned int){return 0;}
+inline int mbedtls_ccm_encrypt_and_tag(mbedtls_ccm_context*,std::size_t len,const unsigned char*,std::size_t,const unsigned char*,std::size_t,const unsigned char* in,unsigned char* out,unsigned char* tag,std::size_t tagLen){for(std::size_t i=0;i<len;++i)out[i]=in[i];for(std::size_t i=0;i<tagLen;++i)tag[i]=0x5A;return 0;}
+inline int mbedtls_ccm_auth_decrypt(mbedtls_ccm_context*,std::size_t len,const unsigned char*,std::size_t,const unsigned char*,std::size_t,const unsigned char* in,unsigned char* out,const unsigned char*,std::size_t){for(std::size_t i=0;i<len;++i)out[i]=in[i];return 0;}
